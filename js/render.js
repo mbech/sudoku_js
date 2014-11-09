ead154d9e11094b42d0c1ee40d6227c23f30f6c8
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-11-08 17:30:07 -0800

    Update cell rendering and board styling
    Add condition to render '0' (i.e. empty) cell values as blank.
    Add method to render board's currently selected cell (apply class);
    Additional board and cell styling.

--- js/render.js
+++ js/render.js
@@ -26,11 +26,23 @@ SUD.render = {
       for(var j=0,len=cellIds.length; j < len; j++){
         var cellId = "cell_" + cellIds[j];
         var $cell = $("<div>", {id: cellId, class: "sudo-cell"}); 
-        $cell.text(board.getCellVal(cellIds[j]));
+
+        var cellValText = board.getCellVal(cellIds[j]);
+        if (cellValText === 0){cellValText = "";}
+
+        $cell.text(cellValText);
         $block.append($cell);
       };
       //cell nodes added to this block, so append it to the board
       $board.append($block); 
     };
+    this.selectedCell(board);
+  },
+
+  selectedCell: function(board){
+    var $cell = $("#cell_" + board.selectedCell);  
+    $(".sudo-cell.is-selected").removeClass("is-selected");
+    $cell.addClass("is-selected");
   }
+
 };
